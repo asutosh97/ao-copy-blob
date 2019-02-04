88c5641d63336357e8380d4fbbff7136ab529f8c
Asutosh Sahoo <asutosh.sahoo@quantiphi.com>
2019-02-04 15:23:21 +0530

    Flexiblity added to run on both local and cloud

--- django_webservice/miscutils/copy_blob.py
+++ django_webservice/miscutils/copy_blob.py
@@ -4,7 +4,10 @@ from . import constants
 
 def copy_blob(source_bucket_name, source_blob_name, destination_bucket_name, destination_blob_name=None):
     try:
-        storage_client = storage.Client.from_service_account_json(constants.KEYPATH)
+        try:
+            storage_client = storage.Client()
+        except Exception as e:
+            storage_client = storage.Client.from_service_account_json(constants.KEYPATH)
         source_bucket = storage_client.get_bucket(source_bucket_name)
         source_blob = source_bucket.get_blob(source_blob_name)
         destination_bucket = storage_client.get_bucket(destination_bucket_name)
@@ -12,4 +15,3 @@ def copy_blob(source_bucket_name, source_blob_name, destination_bucket_name, des
         return copied_blob._properties
     except Exception as e:
         return repr(e)
-        

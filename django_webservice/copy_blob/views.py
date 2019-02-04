e0c0eb1a9292e135c95a59050564ca53a431202c
Asutosh Sahoo <asutosh.sahoo@quantiphi.com>
2019-02-04 14:41:13 +0530

    Removed log upload feature

--- django_webservice/copy_blob/views.py
+++ django_webservice/copy_blob/views.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import json
+import datetime
 
 from django.shortcuts import render
 from django.http import JsonResponse
+
 from miscutils import copy_blob
-from miscutils import log_to_file
-import json
 
 # Create your views here.
 def home(request):
     if request.method == "POST":
         received_json_data = json.loads(request.body.decode('utf-8'))
         response = copy_blob.copy_blob(**received_json_data)
-        log_to_file.main(received_json_data, response)
+
+        # print to console so that its saved in the log file.
+        log_data = {
+                "request": request, 
+                "response": response, 
+                "timestamp":str(datetime.datetime.now())
+        }
+        print("\n\nBlob-Copy-Report :- {}\n\n".format(json.dumps(log_data)))
+        
         success = isinstance(response, dict)
         
         if success:
--- django_webservice/miscutils/constants.py
+++ django_webservice/miscutils/constants.py
@@ -1,3 +1,1 @@
 KEYPATH = "/Users/asutosh97/Documents/keys/athenas-owl-dev-sa.json"
-BUCKET_NAME = "b-ao-intern-test2"
-LOGFILE_NAME = "logfile.txt"
